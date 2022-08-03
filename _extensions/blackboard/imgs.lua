7223bfd2b8a5272ab3a03c81a136401e9f221369
schochastics <david@schochastics.net>
2022-08-03 07:20:07 +0200

    deleted not needed lua

--- _extensions/blackboard/imgs.lua
+++ _extensions/blackboard/imgs.lua
@@ -1,8 +1,3 @@
-local function copyResource(file)
-  path = quarto.utils.resolvePath(file)
-  quarto.doc.addFormatResource(path)
-end
-
 function Header(el)
   if not el.attributes['background-image'] then
     el.attributes['background-image'] = '_extensions/schochastics/blackboard/blackboard.png'
