f68ea9fde9e0195293550ea28283a3f2a82db2c6
schochastics <david@schochastics.net>
2024-02-14 20:54:08 +0100

    change background #2

--- _extensions/blackboard/imgs.lua
+++ _extensions/blackboard/imgs.lua
@@ -1,8 +1,8 @@
 function Header(el)
-  if not el.attributes['background-image'] then
-    el.attributes['background-image'] = '_extensions/schochastics/blackboard/blackboard.png'
-    el.attributes['background-size'] = 'cover'
-  end
-  
-  return el
+    if not el.attributes['background-image'] then
+        el.attributes['background-image'] = '_extensions/blackboard/blackboard.png'
+        el.attributes['background-size'] = 'cover'
+    end
+
+    return el
 end
